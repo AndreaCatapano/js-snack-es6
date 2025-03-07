9be78e983f144ba647e02f645d4810d63b8f966d
AndreaCatapano <a.catapano96@gmail.com>
2025-03-07 15:26:19 +0100

    fix: fixed fo search light weight bike

--- snack1/app.js
+++ snack1/app.js
@@ -3,22 +3,22 @@
 //Stampare a schermo la bici con peso minore.
 
 const bikes = [
-    {nome : "x", peso: 60},
+    {nome : "x", peso: 200},
     {nome : "y", peso: 35},
-    {nome : "w", peso: 40},
+    {nome : "w", peso: 100},
     {nome : "z", peso: 45}
 ]
 
-let maxWeight = 0;
-let heavierBike = 0;
+let minWeight = 1000;
+let lightWeightBike = 0;
 
 for (let i = 0; i < bikes.length; i++){
     const bike = bikes[i];
     const bikeWeight = bike.peso;
-    if (bikeWeight > maxWeight){
-        maxWeight = bikeWeight;
-        heavierBike = i;
+    if (bikeWeight < minWeight){
+        minWeight = bikeWeight;
+        lightWeightBike = i;
     }
 }
 
-console.log(`La bicicletta più pesante è ${bikes[heavierBike].nome} con ${bikes[heavierBike].peso}kg`);
+console.log(`La bicicletta più leggera è ${bikes[lightWeightBike].nome} con ${bikes[lightWeightBike].peso}kg`);
