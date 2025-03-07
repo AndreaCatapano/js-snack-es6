73e35835bb1092c5fac60e923e28a4a54bf9d729
AndreaCatapano <a.catapano96@gmail.com>
2025-03-07 14:29:46 +0100

    feat: Created an array of objects

---
+++ snack1/app.js
@@ -0,0 +1,13 @@
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+const bikes = [
+    {modello : "x", peso: 30},
+    {modello : "y", peso: 35},
+    {modello : "w", peso: 40},
+    {modello : "z", peso: 45}
+]
+
+console.log(bikes[3].modello)
+console.log(bikes[3].peso)
